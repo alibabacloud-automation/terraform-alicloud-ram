0a32c9aaa14a0127fce2e655a2e3f3ee2cc1b733
cyl33922072 <30539836+cyl33922072@users.noreply.github.com>
2019-03-15 10:23:57 -0700

    update example to use final module path

--- examples/ram-user/main.tf
+++ examples/ram-user/main.tf
@@ -6,7 +6,7 @@ provider "alicloud" {
 # RAM user, login profile and access key
 #########################################
 module "ram_user" {
-  source = "../../modules/ram-user"
+  source = "terraform-alicloud-ram/terraform-alicloud-ram/ram-user"
 
   name = "terraformtest1"
 
