ade5ca8774ed189ac971dd0bc8fc07b65c6fc4fc
shanye <shanye.xzq@alibaba-inc.com>
2024-01-15 07:23:50 +0000

    autoSync by Github Action

--- scripts/terraform-test.sh
+++ scripts/terraform-test.sh
@@ -1,57 +1,60 @@
 #!/usr/bin/env sh
 
+if [ ! $# -eq 1 ];then
+  exit 1
+fi
+
 terraformVersionFile="tfversion.md"
 echo "" > $terraformVersionFile
 version=""
 
+exitCode=0
 f=${1}
 success=true
 # echo $f
 echo ""
 echo "====> Terraform testing in" $f
 terraform -chdir=$f init -upgrade >/dev/null
+
+echo ""
+echo "----> Plan Testing"
+terraform -chdir=$f plan >/dev/null
 if [[ $? -ne 0 ]]; then
   success=false
-  echo -e "\033[31m[ERROR]\033[0m: running terraform init failed."
+  exitCode=2
+  echo -e "\033[31m[ERROR]\033[0m: running terraform plan failed."
 else
+  echo -e "\033[32m - plan check: success\033[0m"
   echo ""
-  echo "----> Plan Testing"
-  terraform -chdir=$f plan >/dev/null
+  echo "----> Apply Testing"
+  terraform -chdir=$f apply -auto-approve >/dev/null
+  if [[ $? -ne 0 ]]; then
+      success=false
+      exitCode=3
+      echo -e "\033[31m[ERROR]\033[0m: running terraform apply failed."
+  else
+      echo -e "\033[32m - apply check: success\033[0m"
+      echo ""
+      echo -e " ----> Apply Diff Checking\n"
+      terraform -chdir=$f plan -detailed-exitcode
+      if [[ $? -ne 0 ]]; then
+        success=false
+        exitCode=4
+        echo -e "\033[31m[ERROR]\033[0m: running terraform plan for checking diff failed."
+      else
+        echo -e "\033[32m - apply diff check: success\033[0m"
+      fi
+  fi
+  echo ""
+  echo " ----> Destroying"
+  terraform -chdir=$f destroy -auto-approve >/dev/null 
   if [[ $? -ne 0 ]]; then
     success=false
-    echo -e "\033[31m[ERROR]\033[0m: running terraform plan failed."
+    exitCode=5
+    echo -e "\033[31m[ERROR]\033[0m: running terraform destroy failed."
   else
-    echo -e "\033[32m - plan check: success\033[0m"
-    echo ""
-    echo "----> Apply Testing"
-    terraform -chdir=$f apply -auto-approve >/dev/null
-    if [[ $? -ne 0 ]]; then
-        success=false
-        echo -e "\033[31m[ERROR]\033[0m: running terraform apply failed."
-    else
-        echo -e "\033[32m - apply check: success\033[0m"
-        echo ""
-        echo -e " ----> Apply Diff Checking\n"
-        terraform -chdir=$f plan -detailed-exitcode
-        if [[ $? -ne 0 ]]; then
-          success=false
-          echo -e "\033[31m[ERROR]\033[0m: running terraform plan for checking diff failed."
-        else
-          echo -e "\033[32m - apply diff check: success\033[0m"
-        fi
-    fi
-    echo ""
-    echo " ----> Destroying"
-    terraform -chdir=$f destroy -auto-approve >/dev/null 
-    if [[ $? -ne 0 ]]; then
-      success=false
-      echo -e "\033[31m[ERROR]\033[0m: running terraform destroy failed."
-    else
-      echo -e "\033[32m - destroy: success\033[0m"
-    fi
+    echo -e "\033[32m - destroy: success\033[0m"
   fi
-  rm -rf $f/.terraform
-  rm -rf $f/.terraform.lock.hcl
 fi
 
 version=$(terraform -chdir=$f version)
@@ -63,8 +66,7 @@ fi
 echo -e "### Versions\n" >> $terraformVersionFile 
 echo -e "${version}" >> $terraformVersionFile 
 
-if [[ $success == false ]]; then
-    exit 1
-fi
+rm -rf $f/.terraform
+rm -rf $f/.terraform.lock.hcl
 
-exit 0
+exit $exitCode
